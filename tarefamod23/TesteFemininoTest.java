eb978310d438957bbb8b3b0e5dc9cea6eb7b4887
Erlan Furtado <erlanfurtado7@gmail.com>
2023-05-30 11:15:11 -0300

    Formatado

--- tarefamod23/TesteFemininoTest.java
+++ tarefamod23/TesteFemininoTest.java
@@ -1,11 +1,15 @@
 package tarefamod23;
 
+import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+
 public class TesteFemininoTest {
+    @Test
 
     public void testeFeminino1() {
-
-
-
+        Pessoa pessoa = new Pessoa();
+        pessoa.adicionarSexo("Feminino");
+        Assert.assertEquals("Feminino", pessoa.getSexo());
 
 
     }
